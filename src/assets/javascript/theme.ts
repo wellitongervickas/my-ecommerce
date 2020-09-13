4a19f318a09999ad73ea822dc0bce180215ca8e3
welliton gervickas <wellitogervickas@gmail.com>
2020-09-12 22:13:39 -0300

    styled configuration

---
+++ src/assets/javascript/theme.ts
@@ -0,0 +1,7 @@
+const theme = {
+  primary: '#664EEF',
+  light: '#EEEEEE',
+  dark: '#333333',
+}
+
+export default theme;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'routes';
+import dotenv from 'dotenv';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'assets/javascript/global-styles';
+import theme from 'assets/javascript/theme';
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
